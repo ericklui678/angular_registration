1b7c68d8ffa6ca9ec2e2cc1d68afdd1a32f331d5
Erick Lui <erickjin.lui@gmail.com>
2017-06-14 13:32:35 -0700

    added success confirmation message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 class User {
     constructor(
         public id: number = null,
-        public firstName: string = "",
+        public first_name: string = "",
         public last_name: string = "",
         public email: string = "",
         public password: string = "",
@@ -24,9 +24,11 @@ class User {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  success = '';
   user = new User();
   users = [];
   onSubmit(){
+    this.success = 'Thank you for registering with us ' + this.user.first_name +'. We just sent you a confirmation email at ' + this.user.email + ', and we will send all mail to ' + this.user.address + ', ' + this.user.city + ' ' + this.user.state + '.';
     this.users.push(this.user);
     this.user = new User();
   }
